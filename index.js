5d12fe235737cb81072ef824bfddeaba8dff118b
Mwenda <kelvinfotech@gmail.com>
2024-03-22 15:59:59 +0300

    array lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(Ralph) {
+  cats.push(Ralph);
+}
+
+function destructivelyPrependCat(Bob) {
+  cats.unshift(Bob);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(Broom) {
+  return ["Milo", "Otis", "Garfield", "Broom"];
+}
+
+function prependCat(Arnold) {
+  return ["Arnold", "Milo", "Otis", "Garfield"]
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
